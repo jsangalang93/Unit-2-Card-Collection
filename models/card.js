b307415096ad589a62de5e1638c03c51a23e215f
jsangalang93 <jsangalang93@gmail.com>
2024-05-01 13:53:18 -0400

    add success

---
+++ models/card.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    condition: String,
+    series: String,
+    // isAuth: Boolean
+});
+
+const Cards = mongoose.model('Cards', cardSchema);
+
+module.exports = Cards
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+
+//generates req.body VVVV
+app.use(express.urlencoded({extended:false}))
+
+const Cards = require('./models/card.js');
+
+
+app.get('/', (req, res)=>{
+    res.render('home.ejs')
+})
+
+app.get('/cards/new', (req, res)=>{
+    res.render('cards/new.ejs')
+})
+
+app.get('/cards', async (req, res)=>{
+    const allCards = await Cards.find({})
+    res.render('cards/index.ejs', {
+        cards: allCards
+    })
+})
+
+app.post('/cards', async (req, res)=>{
+    console.log(req.body);
+    // if(req.body.isAuth === 'on'){
+    //     req.body.isAuth = true;
+    // } else {
+    //     req.body.isAuth = false;
+    // }
+    const createdCard = await Cards.create(req.body)
+    res.redirect('/cards')
+})
+
+
+
+
+
+
+mongoose.connection.on('connected', ()=>{
+    console.log(`connected to MongoDB ${mongoose.connection.name}`);
+})
+
+app.listen(3015, () => {
+    console.log('Running on port 3015');
+})
